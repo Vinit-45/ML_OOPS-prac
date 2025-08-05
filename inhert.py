67518a0c9b9bfe7a230bc448be5f44ae8ae1384d
Vinit <vinitmhatre0502@gmail.com>
2025-08-05 13:04:53 +0530

    basics of inheritance

--- inhert.py
+++ inhert.py
@@ -1,16 +1,36 @@
+# class Animal:
+#     def __init__(self,name) :
+#        self.name= name
+
+#     def speak(self):
+#         print(f'{self.name} makes a sound.')
+
+# class Dog(Animal):
+#     def speak(self):
+#         print(f'{self.name} barks.')
+
+# animal = Animal('Generic Animal')
+# animal.speak()
+
+# dog = Dog('Buddy')
+# dog.speak()
+
+
 class Animal:
-    def __init__(self,name) :
-       self.name= name
+    def __init__(self):
+        self.name="Buddy"
 
     def speak(self):
         print(f'{self.name} makes a sound.')
 
 class Dog(Animal):
+    def __init__(self,breed):
+        super().__init__()
+        self.breed = breed
+
     def speak(self):
-        print(f'{self.name} barks.')
+        super().speak()
+        print(f'{self.name} barks. It is a {self.breed}')
 
-animal = Animal('Generic Animal')
-animal.speak()
-
-dog = Dog('Buddy')
+dog = Dog('Husky')
 dog.speak()
